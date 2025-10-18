4aa35a0c8d333621f278b5eb2cb43e180d9d9a58
Priya Singh <priyasingh42613@gmail.com>
2025-10-18 19:59:20 +0530

    to handle get and post request for users

---
+++ media/src/main/java/com/social/media/controllers/SocialController.java
@@ -0,0 +1,36 @@
+package com.social.media.controllers;
+
+import com.social.media.models.SocialUser;
+import com.social.media.services.SocialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class SocialController {
+    @Autowired
+    private SocialService socialService;
+
+    @GetMapping("/social/users")
+    public ResponseEntity<List<SocialUser>> getAllUsers() {
+        List<SocialUser> allUsers = socialService.getAllUsers();
+        return ResponseEntity.ok(allUsers);
+    }
+
+    @PostMapping("/social/users")
+    public ResponseEntity<SocialUser> addUser(@RequestBody SocialUser socialUser) {
+        return new ResponseEntity<>(socialService.addUser(socialUser), HttpStatus.CREATED);
+    }
+
+//    @DeleteMapping("/social/delete/{userId}")
+//    public ResponseEntity<SocialUser> deleteUser(@PathVariable("userId") Long userId) {
+//        SocialUser socialUser = socialService.findById(userId).
+//                orElse(null);
+//
+//        return ResonseEntity.ok(socialUser);
+//    }
+}
