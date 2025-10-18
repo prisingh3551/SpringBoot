37580e531df067bb40dada9952fc9af8b6173d4f
Priya Singh <priyasingh42613@gmail.com>
2025-10-18 19:57:35 +0530

    User to profile and Profile to user has a one-to-one relationship

---
+++ media/src/main/java/com/social/media/models/SocialProfile.java
@@ -0,0 +1,31 @@
+package com.social.media.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+public class SocialProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(mappedBy = "socialProfile")
+    @JsonIgnore
+    private SocialUser socialUser;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
+
