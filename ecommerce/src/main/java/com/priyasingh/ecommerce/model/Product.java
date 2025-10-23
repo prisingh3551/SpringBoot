98011e072c538998c3bdd6bbdc5b50ce33914ea0
Priya Singh <priyasingh42613@gmail.com>
2025-10-23 20:47:41 +0530

    Product Model

---
+++ ecommerce/src/main/java/com/priyasingh/ecommerce/model/Product.java
@@ -0,0 +1,41 @@
+package com.priyasingh.ecommerce.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long productId;
+
+    @NotBlank
+    @Size(min = 3, message = "Product name must contain atleast 3 characters")
+    private String productName;
+
+    private String image;
+
+    @Size(min = 6)
+    private String description;
+
+    @Min(value = 1, message = "Quantity must be at least 1")
+    private Integer quantity;
+
+    @Min(value = 0, message = "Price cannot be negative")
+    private double price;
+    private double discount;
+    private double specialPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
