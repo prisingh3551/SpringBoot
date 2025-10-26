442a63bfd0bfd0b9819c94c7ad6ee84bdca0f73d
Priya Singh <priyasingh42613@gmail.com>
2025-10-26 19:32:10 +0530

    Added role based authorization using @PreAuthorize and @EnableMethodSecurity

--- securitydemo/src/main/java/com/example/securitydemo/GreetingsController.java
+++ securitydemo/src/main/java/com/example/securitydemo/GreetingsController.java
@@ -1,12 +1,26 @@
 package com.example.securitydemo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class GreetingsController {
 
     @GetMapping("/hello")
     public String hello(){
         return "Hello World";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER')")
+    public String userEndPoint(){
+        return "Hello USEr";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminEndPoint(){
+        return "Hello admin";
     }
 }
