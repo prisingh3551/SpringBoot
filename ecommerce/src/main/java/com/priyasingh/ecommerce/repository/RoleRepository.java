c8543d50a82264f1d3a8720690f4d5a748494ffb
Priya Singh <priyasingh42613@gmail.com>
2025-11-02 17:59:42 +0530

    Role repository to organize the users with the given specific role

---
+++ ecommerce/src/main/java/com/priyasingh/ecommerce/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.priyasingh.ecommerce.repository;
+
+import com.priyasingh.ecommerce.model.AppRole;
+import com.priyasingh.ecommerce.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByRoleName(AppRole role);
+}
