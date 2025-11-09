715a8315d1c77cf589cec924a1c47b7f974939b7
Priya Singh <priyasingh42613@gmail.com>
2025-11-09 22:01:45 +0530

    feat(service): define CartService interface with methods for cart management operations

---
+++ ecommerce/src/main/java/com/priyasingh/ecommerce/service/CartService.java
@@ -0,0 +1,19 @@
+package com.priyasingh.ecommerce.service;
+
+import com.priyasingh.ecommerce.payload.CartDTO;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+
+public interface CartService {
+    CartDTO addProductToCart(Long productId, Integer quantity);
+
+    List<CartDTO> getAllCarts();
+
+    CartDTO getCart(String email, Long cartId);
+
+    @Transactional
+    CartDTO updateProductQuantityInCart(Long productId, Integer quantity);
+
+    String deleteProductFromCart(Long cartId, Long productId);
+}
