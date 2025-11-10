4308746626235b309066c167c04f3e312193cd8a
Priya Singh <priyasingh42613@gmail.com>
2025-11-10 20:47:59 +0530

    fix(repository): mark deleteCartItemByProductIdAndCartId as @Modifying
    Added @Modifying to the CartItemRepository delete query so JPA recognizes it as a data modification
    operation. This fixes runtime issues where DELETE queries were ignored or caused exceptions.

--- ecommerce/src/main/java/com/priyasingh/ecommerce/repository/CartItemRepository.java
+++ ecommerce/src/main/java/com/priyasingh/ecommerce/repository/CartItemRepository.java
@@ -2,6 +2,7 @@ package com.priyasingh.ecommerce.repository;
 
 import com.priyasingh.ecommerce.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,7 @@ public interface CartItemRepository extends JpaRepository<CartItem,Long> {
     @Query("SELECT ci FROM CartItem ci WHERE ci.cart.cartId=?1 AND ci.product.productId=?2")
     CartItem findCartItemByProductIdAndCartId(Long cartId, Long productId);
 
+    @Modifying // tell Jpa that we are intending to modify the db
     @Query("DELETE FROM CartItem ci WHERE ci.cart.cartId=?1 AND ci.product.productId=?2")
     void deleteCartItemByProductIdAndCartId(Long cartId, Long productId);
 }
